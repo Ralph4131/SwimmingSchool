41999e35a9dff8643dbcb30d3744a1ab8b941bc3
Ralph4131 <118496908+Ralph4131@users.noreply.github.com>
2023-10-08 05:04:22 +0300

    Update Startup.cs

--- Story 15/Startup.cs
+++ Story 15/Startup.cs
@@ -1,1 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Swimmer
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(100, ErrorMessage = "Name must not exceed 100 characters.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Phone number is required.")]
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "Invalid phone number format.")]
+    public string PhoneNumber { get; set; }
+}
 
